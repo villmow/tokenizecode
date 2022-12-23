1fe11f0667642e1042bf06e65ede07175ec54cfb
Johannes Villmow <johannes.villmow@hs-rm.de>
2022-12-23 10:17:40 +0100

    Add contains to Span

--- tokenizecode/point.py
+++ tokenizecode/point.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from typing import Union
 
 
-@dataclass
+@dataclass(frozen=True)
 class Point:
     """
     Models a point in the text file. Row represents line number and column characters in that line.
@@ -40,7 +41,7 @@ class Point:
         return False
 
 
-@dataclass
+@dataclass(frozen=True)
 class Span:
     """ A span in a code file."""
     start_byte: int
@@ -50,11 +51,16 @@ class Span:
 
     def __post_init__(self):
         if isinstance(self.start_point, (tuple, list)):
-            self.start_point = Point(*self.start_point)
+            object.__setattr__(self, 'start_point', Point(*self.start_point))
         elif isinstance(self.start_point, dict):
-            self.start_point = Point(**self.start_point)
-
-        if isinstance(self.end_point, tuple):
-            self.end_point = Point(*self.end_point)
+            object.__setattr__(self, 'start_point', Point(**self.start_point))
+        if isinstance(self.end_point, (tuple, list)):
+            object.__setattr__(self, 'end_point', Point(*self.end_point))
         elif isinstance(self.end_point, dict):
-            self.end_point = Point(**self.end_point)
+            object.__setattr__(self, 'end_point', Point(**self.end_point))
+
+    def __contains__(self, item: Union[Point, "Span"]) -> bool:
+        if isinstance(item, Span):
+            return item.start_point in self and item.end_point in self
+
+        return self.start_point <= item <= self.end_point
