fc7a1659eefe2507a2a2f5a8eb23496a075533f1
Johannes Villmow <johannes.villmow@hs-rm.de>
2023-05-31 14:56:02 +0200

    add languages to tokenizecode

--- tokenizecode/__init__.py
+++ tokenizecode/__init__.py
@@ -2,6 +2,6 @@ from .utils import get_project_root, adjust_positions
 from .parser import CodeParser, CodeParsingOutput
 from .tokenizer import CodeTokenizer
 from .bpe import SentencePieceBPE, TokenizerBPE
-from .languages import detect_lang, detect_langs, LANGUAGE_MAP, EXTENSION_MAP, get_language_identifier
+from .languages import detect_lang, detect_langs, LANGUAGE_MAP, EXTENSION_MAP, get_language_identifier, LANGUAGES
 from .point import Span, Point
 from .wrapper import WrappedParser
